82b43b67f440143b03ea90bb5c479988d80c4256
DelishAfrica Dev <dev@delish.africa>
2025-10-01 03:50:34 +0200

    fix(health): map GET /api/health

--- services/api/src/modules/health/health.controller.ts
+++ services/api/src/modules/health/health.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Header } from '@nestjs/common';
 
 @Controller('health') // => route finale: /api/health
 export class HealthController {
-  @Get('health')
+  @Get()
   @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0, s-maxage=0')
   @Header('Pragma', 'no-cache')
   @Header('Expires', '0')
